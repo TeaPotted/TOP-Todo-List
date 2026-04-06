8374079399e67eeb222a8f3038d8e8dae967b2c1
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-04-06 20:07:05 +0800

    add a script file that holds a function for creating new list objects and a function that creates an element and sets it's class and textContent

---
+++ src/create-elements.js
@@ -0,0 +1,28 @@
+// an array to keep all the lists
+let library = [];
+
+// create a function that creates a new list object and also pushes it to library
+function List(title) {
+  const list = {title, tasks: []}; // set the list's title and make it have an empty arra to keep the tasks in
+  library.push(list)
+
+  // create a method for adding a task object to the tasks array
+  const addTask = (taskTitle, description, dueDate, priority) => {
+      const task = {taskTitle, description, dueDate, priority}
+
+      list.tasks.push(task)
+    };
+
+  return {addTask}
+};
+
+// create a function for creating elements and adding their class and textContent
+function createElem(element, cls, text) {
+  const elem = document.createElement(element);
+  if (cls !== "") elem.classList.add(cls)
+  elem.textContent = text;
+  return elem
+}
+
+
+export {library, List, createElem}
