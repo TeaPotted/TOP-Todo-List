21a4d8853564f1dca3a25adb1f1e540bf1a8d434
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-04-14 22:52:47 +0800

    refactor List() factory function by making it include the addTask method inside the list object

--- src/create-elements.js
+++ src/create-elements.js
@@ -1,19 +1,18 @@
 // an array to keep all the lists
-let library = [{title: "list1", tasks: [{taskTitle: 'task1', description: 'first task kinda nervous', dueDate: '14:00', priority: 'urgent'}], id: crypto.randomUUID()}];
+let library = [];
+List("poop");
 
 // create a function that creates a new list object and also pushes it to library
 function List(title) {
-  const list = {title, tasks: [], id: crypto.randomUUID()}; // set the list's title and make it have an empty arra to keep the tasks in
-  library.push(list)
-
   // create a method for adding a task object to the tasks array
   const addTask = (taskTitle, description, dueDate, priority) => {
-      const task = {taskTitle, description, dueDate, priority}
+    const task = {taskTitle, description, dueDate, priority}
 
-      list.tasks.push(task)
-    };
+    list.tasks.push(task)
+  };
 
-  return {addTask}
+  const list = {title, tasks: [], id: crypto.randomUUID(), addTask}; // set the list's title and make it have an empty arra to keep the tasks in
+  library.push(list)
 };
 
 // create a function for creating elements and adding their class and textContent
