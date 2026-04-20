0311dd250dc2aa887bea3c9ccf41877b48146287
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-04-20 20:11:37 +0800

    call displayLibrary and make #createList create a new list when clicked

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css"
+import {List} from "./create-elements.js";
+import {displayLibrary} from "./display-elements.js"
+
+
+// get #createList and #listTitle buttons
+const createListBtn = document.getElementById("createList");
+const listTitle = document.getElementById("listTitle");
+
+displayLibrary(); // display the library
+
+// make createListBtn create a new list using listTitle and then display the updated library
+createListBtn.addEventListener("click", () => {
+  List(listTitle.value);
+  displayLibrary();
+});
