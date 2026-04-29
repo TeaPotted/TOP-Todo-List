5cae7a51d816d4357863ee1785928afab98c8210
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-04-29 21:33:01 +0800

    delete stringLibrary() as it is not needed and replace every stringLibrary() call with JSON.stringify(library)

--- src/edit-storage.js
+++ src/edit-storage.js
@@ -1,21 +1,10 @@
 import { library, List, addTaskToList } from "./create-elements.js";
 import { deleteList } from "./delete-things.js";
 
-// create a function that converts library to a string
-function stringLibrary() {
-  const stringedLibrary = JSON.stringify(library, function (key, value) {
-      if (typeof value === "function") {
-        return "" + value;
-      } return value
-    }
-  )
-  return stringedLibrary
-};
-
 // create a function that adds a list to localStorage's 'library' item
 function addListToStorage(listName) {
   List(listName);
-  localStorage.setItem("library", stringLibrary());
+  localStorage.setItem("library", JSON.stringify(library));
 };
 
 // create a function that adds a task to the given list then
@@ -29,7 +18,7 @@ function addTask (list, taskTitle, description, dueDate, priority) {
 function deleteStorageList(list) {
   // delete the list and then update "library" item in localStorage
   deleteList(list);
-  localStorage.setItem("library", JSON.stringify(library, ));
+  localStorage.setItem("library", JSON.stringify(library));
 };
 
 export {addListToStorage, deleteStorageList, addTask}
